bc7aeb6a143f362c6754d67964cbdb708f144022
siirielli <siiri.lietu@gmail.com>
2025-03-04 00:30:19 +0100

    Started on contact component and changed metadata

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,10 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Welcome from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Siiri Lietu Portfolio" },
+    { name: "description", content: "Welcome to my portfolio!" },
   ];
 }
 
