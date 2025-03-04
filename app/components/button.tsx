23b8a25df24686f600a86544de3b7eb532fecb10
siirielli <siiri.lietu@gmail.com>
2025-03-05 00:17:59 +0100

    Add buttons for contact and download CV

---
+++ app/components/button.tsx
@@ -0,0 +1,30 @@
+type ButtonProps = {
+  label: string;
+  link: string;
+};
+
+export default function Button({ label, link }: ButtonProps) {
+  const isPDF = link.endsWith(".pdf");
+
+  return isPDF ? (
+    // If the link is a PDF, use <a> with download attribute
+    <a
+      href={link}
+      download
+      className="bg-primary-pink text-primary-green text-sm font-bold py-1 px-5 rounded-3xl cursor-pointer"
+    >
+      {label}
+    </a>
+  ) : (
+    // If not, scroll smoothly to the section
+    <button
+      onClick={(e) => {
+        e.preventDefault();
+        document.querySelector(link)?.scrollIntoView({ behavior: "smooth" });
+      }}
+      className="bg-primary-pink text-primary-green text-sm font-bold py-1 px-5 rounded-3xl cursor-pointer"
+    >
+      {label}
+    </button>
+  );
+}
---
+++ app/components/nav.tsx
@@ -0,0 +1,13 @@
+import Button from "./button";
+import CVFile from "../assets/siirilietu-cv.pdf";
+
+export default function Nav() {
+  return (
+    <div className="absolute top-0 w-full bg-transparent z-10 px-[var(--spacing-page)] py-5">
+      <nav className="flex justify-end gap-4">
+        <Button link="#contact" label="Contact" />
+        <Button link={CVFile} label="CV" />
+      </nav>
+    </div>
+  );
+}
