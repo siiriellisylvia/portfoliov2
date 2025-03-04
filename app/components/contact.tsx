3330917153074527304d3d74490eee2f4f99cbaa
siirielli <siiri.lietu@gmail.com>
2025-03-05 00:52:21 +0100

    Added contact icons to ContactSection

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,13 +1,28 @@
+import Icon from "./icon";
+
 export default function ContactSection() {
   return (
-    <section className="w-full relative z-3 flex flex-col items-center text-center h-screen">
-      <div className="mt-5">
-        <h2 className="mt-0 text-4xl font-extrabold font-heading">Am I the colleague you're looking for?</h2>
-        <p className="font-bold">
-          I'm always happy to grab a coffee and talk about it!
-          <br></br>You can also email me.
-        </p>
+    <>
+      <section
+        id="contact"
+        className="w-full relative z-3 flex flex-col items-center text-center h-screen p-[var(--spacing-page)] bg-light-pink"
+      >
+        <div>
+          <h2 className="mt-0 text-4xl font-extrabold font-heading">
+            Am I the colleague you're looking for?
+          </h2>
+          <p className="font-bold">
+            I'm always happy to grab a coffee and talk about it!
+            <br></br>You can also email me.
+          </p>
         </div>
-        </section>
-    );
+        <div className="flex flex-row gap-6">
+          <Icon name="email" />
+          <Icon name="phone" />
+          <Icon name="linkedin" />
+          <Icon name="github" />
+        </div>
+      </section>
+    </>
+  );
 }
