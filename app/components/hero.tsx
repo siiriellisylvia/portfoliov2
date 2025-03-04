96ca9c121d14b48eaf1c387f3c052d3da1d5bac8
siirielli <siiri.lietu@gmail.com>
2025-03-04 16:22:27 +0100

    Add image to hero section and tweak layout

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,37 +1,48 @@
 import Icon from "./icon";
+import heroImage from "../assets/hero-image.webp";
+import Stars from "./stars";
 
 export default function HeroSection() {
   return (
-    <section className="w-full relative z-3 flex flex-col h-screen">
-      <div className="h-1/2 flex flex-col justify-end pb-5 px-[var(--spacing-page)]">
-        <h2>Nice to meet you!</h2>
-        <h1>I'm Siiri</h1>
-      </div>
-      <div className="h-1/2 py-5 bg-primary-green text-primary-pink px-[var(--spacing-page)]">
-        <h2>a frontend developer.</h2>
-        <p>
-          I'm web development student looking for an internship for fall 2025.
-        </p>
-        <p className="mobile">I know a thing or two about all of these:</p>
-        <div className="flex flex-row gap-2 my-5">
-          <Icon name="CSS" />
-          <Icon name="javaScript" />
-          <Icon name="react" />
-          <Icon name="react-router" />
-          <Icon name="typescript" />
-          <Icon name="tailwind-CSS" />
-          <Icon name="mongoDB" />
-          <Icon name="mySQL" />
-          <Icon name="next.js" />
-          <Icon name="node" />
-          <Icon name="php" />
-          <Icon name="webflow" />
-          <Icon name="wordpress" />
-          <Icon name="git" />
-          <Icon name="jira" />
-          <Icon name="figma" />
+    <main className="w-full relative z-3 flex flex-row h-screen">
+      <section className="w-2/3 relative z-3 flex flex-col h-screen  bg-primary-green text-primary-pink">
+        <div className="h-3/5 flex flex-col justify-end pb-5 px-[var(--spacing-page)] relative overflow-hidden">
+          <h2>Nice to meet you!</h2>
+          <h1>I'm Siiri,</h1>
         </div>
-      </div>
-    </section>
+        <div className="h-2/5 py-5 bg-primary-pink text-primary-green px-[var(--spacing-page)]">
+          <h2>a frontend developer.</h2>
+          <p>
+            I'm web development student looking for an internship for fall 2025.
+          </p>
+          <p className="mobile">I know a thing or two about all of these:</p>
+          <div className="flex flex-wrap gap-2 my-5">
+            <Icon name="CSS" />
+            <Icon name="javaScript" />
+            <Icon name="react" />
+            <Icon name="react-router" />
+            <Icon name="typescript" />
+            <Icon name="tailwind-CSS" />
+            <Icon name="mongoDB" />
+            <Icon name="mySQL" />
+            <Icon name="next.js" />
+            <Icon name="node" />
+            <Icon name="php" />
+            <Icon name="webflow" />
+            <Icon name="wordpress" />
+            <Icon name="git" />
+            <Icon name="jira" />
+            <Icon name="figma" />
+          </div>
+        </div>
+      </section>
+      <section className="w-1/3 relative z-3 flex flex-col h-full ">
+        <img
+          src={heroImage}
+          alt="Siiri Lietu"
+          className="object-cover h-full"
+        />
+      </section>
+    </main>
   );
 }
