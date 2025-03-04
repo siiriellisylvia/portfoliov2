d28cc9174be6064a2c686445170fd4bf5d235e87
siirielli <siiri.lietu@gmail.com>
2025-03-04 15:16:52 +0100

    Adjust styling and rename component

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,8 +1,8 @@
-export default function Contact() {
+export default function ContactSection() {
   return (
-    <section className="w-full relative z-3 flex flex-col items-center text-center h-screen bg-amber-200">
+    <section className="w-full relative z-3 flex flex-col items-center text-center h-screen">
       <div className="mt-5">
-        <h2 className="mt-0 text-4xl font-extrabold">Am I the colleague you're looking for?</h2>
+        <h2 className="mt-0 text-4xl font-extrabold font-heading">Am I the colleague you're looking for?</h2>
         <p className="font-bold">
           I'm always happy to grab a coffee and talk about it!
           <br></br>You can also email me.
