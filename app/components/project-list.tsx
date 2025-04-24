9cc62a587eb2159db06ddfb0e52b9e6f2955a480
siirielli <siiri.lietu@gmail.com>
2025-04-24 23:20:09 +0200

    Refactor ProjectsList component to load projects from the database and update project key in links

--- app/components/project-list.tsx
+++ app/components/project-list.tsx
@@ -1,15 +1,27 @@
-// src/components/ProjectsList.tsx
-import { Link } from "react-router";
-import { projects } from "~/data/projects";
 
-export default function ProjectsList() {
+import Project, { type ProjectType } from "~/db/models/Project";
+import type { Route } from "../types/root";
+import { Link } from "react-router";
+
+export async function loader() {
+  const projects = await Project.find({}).sort({ createdAt: -1 });
+  if (!projects) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  console.log("projects", projects);
+  return projects || [];
+
+}
+export default function ProjectsList({ loaderData }: Route.ComponentProps) {
+  const projects = (loaderData || []) as ProjectType[];
+
   return (
     <section className="lg:w-full px-[var(--spacing-page)] py-10 sm:px-0">
       <div className="flex flex-row gap-6">
         {projects.map((project) => (
           <Link
-            key={project.id}
-            to={`/projects/${project.id}`}
+            key={project.title}
+            to={`/projects/${project.title}`}
             className=" p-4 bg-primary-green text-primary-pink rounded-lg"
           >
             <img
