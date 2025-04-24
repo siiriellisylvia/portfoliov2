ccbe687655c792a4d36194162582e4ddb4d740ad
siirielli <siiri.lietu@gmail.com>
2025-04-24 22:47:57 +0200

    Add database connection and project model; update dependencies

---
+++ app/db/database.server.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+export default function connectDb() {
+  if (process.env.NODE_ENV === "development") {
+    mongoose.set("overwriteModels", true);
+  }
+
+  const readyState = mongoose.connection.readyState;
+  if (readyState > 0) {
+    console.log(
+      "Mongoose: Re-using existing connection (readyState: %d)",
+      readyState
+    );
+    return;
+  }
+
+  mongoose.connection.on("error", (error: any) => {
+    console.error("Mongoose: error %s", error);
+  });
+
+  for (const event of ["connected", "reconnected", "disconnected", "close"]) {
+    mongoose.connection.on(event, () => console.log("Mongoose: %s", event));
+  }
+
+  if (!process.env.MONGODB_URL) {
+    throw new Error("MONGODB_URL environment variable is not defined");
+  }
+  mongoose.connect(process.env.MONGODB_URL).catch((error: any) => {
+    console.error(error);
+  });
+}
---
+++ app/db/models/Project.ts
@@ -0,0 +1,43 @@
+import { Schema, model, Types, type InferSchemaType } from "mongoose";
+
+// Define the schema for the Project collection in MongoDB
+const projectSchema = new Schema(
+  {
+    title: {
+      type: String
+    },
+    subtitle: {
+      type: String
+    },
+    date: {
+      type: String
+    },
+    duration: {
+      type: String
+    },
+    type: {
+      type: String
+    },
+    tech: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    image: {
+      type: String
+    },
+    liveLink: {
+      type: String
+    },
+  },
+  { timestamps: true }, // Automatically add `createdAt` and `updatedAt`
+);
+
+export type ProjectType = InferSchemaType<typeof projectSchema> & {
+  _id: Types.ObjectId | string;
+};
+
+const Project = model<ProjectType>("Project", projectSchema);
+
+export default Project;
