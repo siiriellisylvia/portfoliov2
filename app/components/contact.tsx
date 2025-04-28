b1e5643a3282cb67c263402551a296595509a4dd
siirielli <siiri.lietu@gmail.com>
2025-04-29 00:00:20 +0200

    refactor: add contact details in contact section

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import Icon from "./icon";
 
 export default function ContactSection() {
@@ -5,22 +6,29 @@ export default function ContactSection() {
     <>
       <section
         id="contact"
-        className="w-full relative z-3 flex flex-col justify-center items-center text-center h-screen px-[var(--spacing-page-mobile)] md:[var(--spacing-page)] bg-primary-pink"
+        className="w-full relative z-3 gap-6 flex flex-col justify-center items-center text-center h-screen px-[var(--spacing-page-mobile)] md:[var(--spacing-page)] bg-primary-pink"
       >
         <div>
-          <h2 className="mt-0 text-4xl font-extrabold font-heading">
-            Am I the colleague you're looking for?
-          </h2>
+          <h2>Am I the colleague you're looking for?</h2>
           <p className="font-bold">
             I'm always happy to grab a coffee and talk about it!
-            <br></br>You can also email me.
           </p>
         </div>
-        <div className="flex flex-row gap-6">
-          <Icon name="email" />
-          <Icon name="phone" />
-          <Icon name="linkedin" />
-          <Icon name="github" />
+        <div className="flex flex-row gap-12">
+          <Link to="mailto:siiri.lietu@gmail.com" className="text-sm">
+            <Icon name="email" />
+            siiri.lietu@gmail.com
+          </Link>
+          <div>
+            <Icon name="phone" />
+            <p className="text-sm!">42464737</p>
+          </div>
+          <Link to="https://www.linkedin.com/in/siiri-lietu-b3375387/">
+            <Icon name="linkedin" />
+          </Link>
+          <Link to="https://github.com/siiriellisylvia">
+            <Icon name="github" />
+          </Link>
         </div>
       </section>
     </>
