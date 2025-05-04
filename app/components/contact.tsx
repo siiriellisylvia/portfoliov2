d47a8e411d925a34512284423586b59415bd3815
siirielli <siiri.lietu@gmail.com>
2025-05-04 10:11:14 +0200

    refactor: adjust contact section icon styles

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -15,20 +15,28 @@ export default function ContactSection() {
           </p>
         </div>
         <div className="flex flex-row gap-12">
-          <Link to="mailto:siiri.lietu@gmail.com" className="text-sm">
-            <Icon name="email" />
-            siiri.lietu@gmail.com
-          </Link>
-          <div>
-            <Icon name="phone" />
-            <p className="text-sm!">42464737</p>
+          <div className="flex flex-col items-center">
+            <Link to="mailto:siiri.lietu@gmail.com">
+              <Icon name="email" className="h-16 w-16 md:w-24 md:h-24 fill-primary-pink bg-primary-green p-2 rounded" hideLabel={true} />
+            </Link>
+            <p className="text-sm mt-2">siiri.lietu@gmail.com</p>
           </div>
-          <Link to="https://www.linkedin.com/in/siiri-lietu-b3375387/">
-            <Icon name="linkedin" />
-          </Link>
-          <Link to="https://github.com/siiriellisylvia">
-            <Icon name="github" />
-          </Link>
+          <div className="flex flex-col items-center">
+            <Icon name="phone" className="h-16 w-16 md:w-24 md:h-24 fill-primary-pink bg-primary-green p-2 rounded" hideLabel={true} />
+            <p className="text-sm mt-2">42464737</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Link to="https://www.linkedin.com/in/siiri-lietu-b3375387/">
+              <Icon name="linkedin" className="h-16 w-16 md:w-24 md:h-24 fill-primary-green bg-primary-green p-2 rounded" hideLabel={true} />
+            </Link>
+            <p className="text-sm mt-2">LinkedIn</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Link to="https://github.com/siiriellisylvia">
+              <Icon name="github" className="h-16 w-16 md:w-24 md:h-24 fill-primary-pink bg-primary-green p-2 rounded" hideLabel={true} />
+            </Link>
+            <p className="text-sm mt-2">Github</p>
+          </div>
         </div>
       </section>
     </>
